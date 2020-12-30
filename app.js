5c21fc3d865675ac22e1d175557d504f17a1599e
Annie Do <annie.do247@gmail.com>
2020-12-30 22:40:28 +0700

    add timeAgo

--- app.js
+++ app.js
@@ -10,22 +10,25 @@ async function update() {
 }
 
 function renderArticleCard(article) {
+   let timeAgo= moment(article.publishedAt).fromNow();
   return `
-    <div class="card mb-3 mt-3">
+    <div class="card mb-3 mt-5">
       <img src="${article.urlToImage}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${article.title}</h5>
         <p class="card-text">${article.description}</p>
+        <p  class="card-text" id = "time">${timeAgo}</p>
       </div>
     </div>
   `;
-}
+ }
 
 function render() {
   let resultsArea = document.getElementById('results');
   resultsArea.innerHTML = newsArticles
     .map((article) => renderArticleCard(article))
     .join('');
+  
 }
 
 function topHeadlinesClicked() {
@@ -44,3 +47,4 @@ function everythingClicked() {
 }
 
 update();
+
