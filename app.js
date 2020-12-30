512f5f0074680f4b6ff0b9f42d3e72c281bb8463
nhamhung <nhamhung.gttn@gmail.com>
2020-12-30 21:19:56 +0800

    first commit for template

---
+++ app.js
@@ -0,0 +1,46 @@
+let urlEndpoint =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=1e34783b3b774ec5bdea26967ca444f0';
+let newsArticles = [];
+
+async function update() {
+  let response = await fetch(urlEndpoint);
+  let data = await response.json();
+  newsArticles = data.articles;
+  render();
+}
+
+function renderArticleCard(article) {
+  return `
+    <div class="card mb-3 mt-3">
+      <img src="${article.urlToImage}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${article.title}</h5>
+        <p class="card-text">${article.description}</p>
+      </div>
+    </div>
+  `;
+}
+
+function render() {
+  let resultsArea = document.getElementById('results');
+  resultsArea.innerHTML = newsArticles
+    .map((article) => renderArticleCard(article))
+    .join('');
+}
+
+function topHeadlinesClicked() {
+  console.log('Top headlines');
+  urlEndpoint =
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=1e34783b3b774ec5bdea26967ca444f0';
+  update();
+}
+
+function everythingClicked() {
+  console.log('Everything');
+
+  urlEndpoint =
+    'https://newsapi.org/v2/everything?q=bitcoin&apiKey=1e34783b3b774ec5bdea26967ca444f0';
+  update();
+}
+
+update();
